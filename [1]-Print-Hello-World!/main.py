172bead22990bd3da61f107c16fb07bcf9922c59
Subham Sahu <about.subhamsahu@gmail.com>
2022-05-30 19:39:04 +0530

    30

--- [1]-Print-Hello-World!/main.py
+++ [1]-Print-Hello-World!/main.py
@@ -1,5 +1,19 @@
 # This is a single-line comment in python
 
+"""
+This is a
+multiline
+comment with 
+double-quotes
+"""
+
+'''
+This is a
+multiline
+comment with 
+single-quotes
+'''
+
 # print function with single-quotes and double-quotes
 
 print("\nHi!");
