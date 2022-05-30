c671ca44f1ebddc61cbe638284da4a1b744f7e21
Subham Sahu <about.subhamsahu@gmail.com>
2022-05-30 20:25:58 +0530

    30

---
+++ [2]-Add-Two-Numbers/main.py
@@ -0,0 +1,28 @@
+# QUESTION Add Two Given Numbers
+
+num1 = 2;
+num2 = 5;
+
+# sum of two integers
+sum1 = num1 + num2;
+
+print("\nThe sum of {0} & {1} is = {2}" .format(num1, num2, sum1));
+
+num3 = 3.7;
+
+# sum of two decimals
+sum2 = num2 + num3;
+
+print("\nThe sum of {0} & {1} is = {2}" .format(num2, num3, sum2));
+
+# --------------------------------------------------
+
+# QUESTION Add Two User Inputed Numbers
+
+# Stores input numbers
+numX = input("\nEnter X = ");
+numY = input("\nEnter Y = ");
+
+summ = int(float(numX) + float(numY));
+
+print("\nThe sum of {0} & {1} is = {2}".format(numX, numY, summ)+"\n");
